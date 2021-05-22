b12810d82c9c4f0d46afe5c573bfe6a8b1e2aa4b
anniequ <jiangping_qu@126.com>
2021-05-22 20:04:16 +0800

    finish goods parameters function

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -2,7 +2,8 @@ import Vue from 'vue'
 import {
     Button, Form, FormItem, Input, Message, Container, Header, Aside, Main, Menu, Submenu,
     MenuItemGroup, MenuItem, Breadcrumb, BreadcrumbItem, Card, Row, Col, Table, TableColumn,
-    Switch, Tooltip, Pagination, Dialog, MessageBox, Tag, Tree, Select, Option, Cascader
+    Switch, Tooltip, Pagination, Dialog, MessageBox, Tag, Tree, Select, Option, Cascader,
+    Alert, Tabs, TabPane
 } from 'element-ui'
 
 Vue.use(Button)
@@ -33,5 +34,8 @@ Vue.use(Tree)
 Vue.use(Select)
 Vue.use(Option)
 Vue.use(Cascader)
+Vue.use(Alert)
+Vue.use(Tabs)
+Vue.use(TabPane)
 Vue.prototype.$message = Message  //导入弹框提示
 Vue.prototype.$confirm = MessageBox.confirm  //导入确认弹框提示
